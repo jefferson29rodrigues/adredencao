892a39ed97ba286579f5351e99ce85fe98985e70
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-03-09 14:52:50 -0300

    sevidor rodando e pagina index ok

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,160 +1,160 @@
-const { age, date } = require('./utils')
+// const { age, date } = require('./utils')
 
-// index 
-exports.index = function(req, res) {
-    
-    const membros = ['Jeff', 'juh'];
-
-    return res.render("members", {membros})
-}
-
-//show
-exports.show = function(req, res) {
-    // req.query.id
-    // req.body
-    // req.params.id = /:id
-
-    const { id } = req.params
-
-    const foundInstructor = data.instructors.find(function(instructor) {
-        return id == instructor.id
-    })
-
-    if (!foundInstructor) return res.send("Instructor not found!")
-
-    
-
-    const instructor = {
-      ...foundInstructor,
-      age: age(foundInstructor.birth),
-      services: foundInstructor.services.split(","),
-      created_at: new Intl.DateTimeFormat("en-GB").format(foundInstructor.created_at),
+// // index 
+module.exports = {
+     index(req, res){
+        
+        return res.render("members/index")
     }
-
-    return res.render("instructors/show", { instructor })
-
 }
 
-// create
-exports.post = function(req, res) {
+// //show
+// exports.show = function(req, res) {
+//     // req.query.id
+//     // req.body
+//     // req.params.id = /:id
 
-    const keys = Object.keys(req.body)
+//     const { id } = req.params
 
-   for(key of keys) {
-       //req.body.avatar_url == "";
-       if (req.body[key] == "") {
-           return res.send('Please, fill all')
-       }
-   }
+//     const foundInstructor = data.instructors.find(function(instructor) {
+//         return id == instructor.id
+//     })
 
-   let {
-       image,
-       birth, 
-       gender, 
-       services,
-       nome,
-       endereco,
-       numero,
-       complemento,
-       bairro,
-       cep,
-       cidade,
-       estado,
-       pais,
-       nomePai,
-       nomeMae,
-       sexo,
-       rg,
-       cpf,
-       dataNascimento,
-       batismo,
-       uniao,
-       estadoCivil,
-       profissao,
-       empregado,
-       escolaridade,
-       email,
-       foneResidencial,
-       foneComercial,
-       celular,
-       foneRecados,
-       veioDe,
-       posicao,
-       eDirigente
-    } = req.body
+//     if (!foundInstructor) return res.send("Instructor not found!")
 
-   birth = Date.parse(birth)
-   const created_at = Date.now()
-   const id = Number(data.instructors.length + 1)
+    
+
+//     const instructor = {
+//       ...foundInstructor,
+//       age: age(foundInstructor.birth),
+//       services: foundInstructor.services.split(","),
+//       created_at: new Intl.DateTimeFormat("en-GB").format(foundInstructor.created_at),
+//     }
+
+//     return res.render("instructors/show", { instructor })
+
+// }
+
+// // create
+// exports.post = function(req, res) {
+
+//     const keys = Object.keys(req.body)
+
+//    for(key of keys) {
+//        //req.body.avatar_url == "";
+//        if (req.body[key] == "") {
+//            return res.send('Please, fill all')
+//        }
+//    }
+
+//    let {
+//        image,
+//        birth, 
+//        gender, 
+//        services,
+//        nome,
+//        endereco,
+//        numero,
+//        complemento,
+//        bairro,
+//        cep,
+//        cidade,
+//        estado,
+//        pais,
+//        nomePai,
+//        nomeMae,
+//        sexo,
+//        rg,
+//        cpf,
+//        dataNascimento,
+//        batismo,
+//        uniao,
+//        estadoCivil,
+//        profissao,
+//        empregado,
+//        escolaridade,
+//        email,
+//        foneResidencial,
+//        foneComercial,
+//        celular,
+//        foneRecados,
+//        veioDe,
+//        posicao,
+//        eDirigente
+//     } = req.body
+
+//    birth = Date.parse(birth)
+//    const created_at = Date.now()
+//    const id = Number(data.instructors.length + 1)
 
    
 
-   // na 1° [] -> [{...}] na 2° [{...}] -> [{...}, {...}] 
-   data.instructors.push({
-        id,
-        image,
-        birth,
-        gender,
-        services,
-        created_at,
-        nome,
-        endereco,
-        numero,
-        complemento,
-        bairro,
-        cep,
-        cidade,
-        estado,
-        pais,
-        nomePai,
-        nomeMae,
-        sexo,
-        rg,
-        cpf,
-        dataNascimento,
-        batismo,
-        uniao,
-        estadoCivil,
-        profissao,
-        empregado,
-        escolaridade,
-        email,
-        foneResidencial,
-        foneComercial,
-        celular,
-        foneRecados,
-        veioDe,
-        posicao,
-        eDirigente
-   }) //  na 3° [{...}, {...}] -> [{...}, {...}, {...}]
+//    // na 1° [] -> [{...}] na 2° [{...}] -> [{...}, {...}] 
+//    data.instructors.push({
+//         id,
+//         image,
+//         birth,
+//         gender,
+//         services,
+//         created_at,
+//         nome,
+//         endereco,
+//         numero,
+//         complemento,
+//         bairro,
+//         cep,
+//         cidade,
+//         estado,
+//         pais,
+//         nomePai,
+//         nomeMae,
+//         sexo,
+//         rg,
+//         cpf,
+//         dataNascimento,
+//         batismo,
+//         uniao,
+//         estadoCivil,
+//         profissao,
+//         empregado,
+//         escolaridade,
+//         email,
+//         foneResidencial,
+//         foneComercial,
+//         celular,
+//         foneRecados,
+//         veioDe,
+//         posicao,
+//         eDirigente
+//    }) //  na 3° [{...}, {...}] -> [{...}, {...}, {...}]
 
-   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-       if (err) return res.send("write file error!")
+//    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+//        if (err) return res.send("write file error!")
 
-       return res.redirect("/members")
-   });
+//        return res.redirect("/members")
+//    });
 
-    //return res.send(req.body)
-}
+//     //return res.send(req.body)
+// }
 
-// edit
-exports.edit = function(req, res) {
-    const { id } = req.params
+// // edit
+// exports.edit = function(req, res) {
+//     const { id } = req.params
 
-    const foundInstructor = data.instructors.find(function(instructor) {
-        return id == instructor.id
-    })
+//     const foundInstructor = data.instructors.find(function(instructor) {
+//         return id == instructor.id
+//     })
 
-    if (!foundInstructor) return res.send("Instructor not found!")
+//     if (!foundInstructor) return res.send("Instructor not found!")
 
-    const instructor = {
-        ...foundInstructor,
-        birth: date(foundInstructor.birth) // 2000-2-1
-    }
+//     const instructor = {
+//         ...foundInstructor,
+//         birth: date(foundInstructor.birth) // 2000-2-1
+//     }
 
-    return res.render('instructors/edit', { instructor })
-}
+//     return res.render('instructors/edit', { instructor })
+// }
 
-// update
+// // update
 
-// delete
+// // delete
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const routes = express.Router();
-const members = require('./members');
+const members = require('./app/controllers/members');
 
 routes.get('/', function(req, res) {
-    return res.redirect('members')
+    return res.redirect('/members')
 });
 
-routes.get('/members/create', function(req, res) {
-    return res.render('members/create')
-});
+// routes.get('/members/create', function(req, res) {
+//     return res.render('members/create')
+// });
 
 routes.get('/members', members.index)
 
-routes.get('/members/:id', members.show)
+// routes.get('/members/:id', members.show)
 
-routes.get('/members/:id/edit', members.edit)
+// routes.get('/members/:id/edit', members.edit)
 
-routes.post('/members', members.post);
+// routes.post('/members', members.post);
 
 module.exports = routes;
